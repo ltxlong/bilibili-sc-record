bef648d053f3a563cdfbfe4f45d0ed42b261f0ea
ltxlong <416803647@qq.com>
2024-12-25 22:23:22 +0800

    Update bilibili_sc_record.js

--- bilibili_sc_record.js
+++ bilibili_sc_record.js
@@ -2,7 +2,7 @@
 // @name         B站直播间SC记录板
 // @namespace    http://tampermonkey.net/
 // @homepage     https://greasyfork.org/zh-CN/scripts/484381
-// @version      12.3.1
+// @version      12.3.2
 // @description  实时同步SC、同接、高能和舰长数据，可拖拽移动，可导出，可单个SC折叠，可侧折，可搜索，可记忆配置，可生成图片（右键菜单），活动页可用，直播全屏可用，黑名单功能，不用登录，多种主题切换，自动清除超过12小时的房间SC存储，可自定义SC过期时间，可指定用户进入直播间提示、弹幕高亮和SC转弹幕，可让所有的实时SC以弹幕方式展现，可自动点击天选，可自动跟风发送combo弹幕
 // @author       ltxlong
 // @match        *://live.bilibili.com/1*
@@ -6007,7 +6007,7 @@
                     if (the_sc_panel_show_time_sc_and_most_second_val < 5) {
                         the_sc_panel_show_time_sc_and_most_second_val = 5;
                     }
-                    sc_panel_show_time_each_same = parseFloat((parseInt(the_sc_panel_show_time_sc_and_most_second_val, 10)/60).toFixed(2));
+                    sc_panel_show_time_each_same = parseFloat((parseInt(the_sc_panel_show_time_sc_and_most_second_val, 10)/60).toFixed(3));
                 } else {
                     sc_panel_show_time_each_same = 0.1;
                 }
@@ -6055,7 +6055,7 @@
                     if (the_sc_panel_show_time_sc_and_most_second_val < 5) {
                         the_sc_panel_show_time_sc_and_most_second_val = 5;
                     }
-                    sc_panel_show_time_each_same = parseFloat((parseInt(the_sc_panel_show_time_sc_and_most_second_val, 10)/60).toFixed(2));
+                    sc_panel_show_time_each_same = parseFloat((parseInt(the_sc_panel_show_time_sc_and_most_second_val, 10)/60).toFixed(3));
                 } else {
                     sc_panel_show_time_each_same = 0.1;
                 }
@@ -8689,7 +8689,7 @@
             }
 
             if (sc_panel_show_time_mode === 5) {
-                $(document).find('#' + sc_live_panel_show_time_sc_and_most_second_input_id).val(parseInt(the_sc_panel_show_time_each_same * 60));
+                $(document).find('#' + sc_live_panel_show_time_sc_and_most_second_input_id).val(Math.round(the_sc_panel_show_time_each_same * 60));
             } else {
                 $(document).find('#' + sc_live_panel_show_time_sc_input_id).val(the_sc_panel_show_time_each_same);
                 $(document).find('#' + sc_live_panel_show_time_sc_and_most_time_input_id).val(the_sc_panel_show_time_each_same);
